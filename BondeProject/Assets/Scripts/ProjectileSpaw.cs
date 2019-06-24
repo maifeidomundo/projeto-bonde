8b04b5e98e5eea0e189b5a8aba5c2823095acc27
maifeidomundo <maifeidomundo@gmail.com>
2019-06-24 11:01:23 -0300

    Projetil

--- BondeProject/Assets/Scripts/ProjectileSpaw.cs
+++ BondeProject/Assets/Scripts/ProjectileSpaw.cs
@@ -2,17 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ProjectileSpaw : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+ 
+    
+
+
+    GameObject parent;
+    public GameObject prefab;
+    public GameObject spawnPoint;
+    public float waitTime = 3f;
+    public float projectileSpeed = 3f;
+
+    public Vector3[] directions;
+
     void Start()
     {
-        
+        parent = GameObject.Find("Tape");
+        StartCoroutine(Spawn());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Spawn()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            for (int i = 0; i < directions.Length; i++)
+            {
+                GameObject projectile = Instantiate(prefab, spawnPoint.transform.position, Quaternion.identity);
+                projectile.transform.SetParent(parent.transform);
+                projectile.GetComponent<Rigidbody2D>().velocity = projectileSpeed * directions[i];
+            }
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
