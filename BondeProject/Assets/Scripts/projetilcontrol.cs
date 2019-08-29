55bfb273795dccd034321850446f2fea5b838135
maifeidomundo <maifeidomundo@gmail.com>
2019-08-29 11:22:09 -0300

    Moisezão é top

---
+++ BondeProject/Assets/Scripts/projetilcontrol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projetilcontrol : MonoBehaviour
+{
+    public GameObject prefab;
+   
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemies"))
+        {
+            Destroy(gameObject);
+
+
+        }
+
+    }
+    }
